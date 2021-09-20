8123db19581dcc6b97d739058c5f7998cf95ccc6
Sergey <sshchesnyak@gmail.com>
2021-09-20 15:28:32 +0300

    HW1_COMMIT

---
+++ sshchesnyak_kts_android_09_2021/app/src/main/java/ru/iu3/sshchesnyak_kts_android_09_2021/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.iu3.sshchesnyak_kts_android_09_2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
